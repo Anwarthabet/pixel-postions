f88fc0b02875ab01946b374cff0ca4a02eaa3ba1
Anwar Thabet <anwar773bs@gmail.com>
2025-08-22 17:39:28 +0300

    Your commit message here

---
+++ pixel-postions/resources/views/components/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel Positions</title>
+    {{-- استدعاء الملفات عبر Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div>
+        <nav class="flex justify-between items-center p-4 bg-gray-800 text-white"  >
+         
+        <div>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Logo">
+            </a>
+        </div>
+        <div class="flex space-x-6">
+            <a href="#">Jobs</a>
+            <a href="#">careers</a>  
+            <a href="#">Salaries</a>  
+            <a href="#">Companies</a>      
+        </div>
+        <div>
+            <a href="#">Post Job</a>
+        </div>
+        </nav>
+    </div>
+
+    <div>
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
